2cfe3b4ad3fe5e7460416df15e88a96b4b5d2ecb
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2021-10-14 11:50:26 +0200

    Update perl install paths

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,8 +12,7 @@ fi
   --enable-svnxx \
   --enable-bdb6 \
   --with-sqlite="${PREFIX}" \
-  --disable-static \
-  --with-swig-perl=${BUILD_PREFIX}/bin/perl
+  --disable-static
 
 make -j ${CPU_COUNT}
 make -j ${CPU_COUNT} check CLEANUP=true TESTS=subversion/tests/cmdline/basic_tests.py
@@ -22,6 +21,12 @@ make install
 make swig-pl-lib
 make install-swig-pl-lib
 pushd subversion/bindings/swig/perl/native
-${BUILD_PREFIX}/bin/perl Makefile.PL INSTALLDIRS=site
+# The currently pinned perl version 5.26.2 uses the following layout.
+# This is subject to change on updates of perl.
+# lib/site_perl/5.26.2/x86_64-linux-thread-multi
+# lib/site_perl/5.26.2
+# lib/5.26.2/x86_64-linux-thread-multi
+# lib/5.26.2
+perl Makefile.PL PREFIX="${PREFIX}" INSTALLDIRS=site INSTALLARCHLIB="${PREFIX}/lib/5.26.2" INSTALLSITEARCH="${PREFIX}/lib/site_perl/5.26.2"
 make install
 popd
