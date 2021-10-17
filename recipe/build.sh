7a90a9de54b94207819ebe83349fe8159540eda0
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2021-10-17 12:54:33 +0200

    Use $PERL to try and get macos going

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,13 +18,9 @@ make -j ${CPU_COUNT}
 make -j ${CPU_COUNT} check CLEANUP=true TESTS=subversion/tests/cmdline/basic_tests.py
 make install
 
-if [[ -z "${MACOSX_DEPLOYMENT_TARGET}" ]] ; then
-    # On Linux, build the perl bindings;
-    # on macosx, they lead to a segfault at the moment.
-    make swig-pl-lib
-    make install-swig-pl-lib
-    pushd subversion/bindings/swig/perl/native
-    ${PREFIX}/bin/perl Makefile.PL INSTALLDIRS=site
-    make install
-    popd
-fi
+make swig-pl-lib
+make install-swig-pl-lib
+pushd subversion/bindings/swig/perl/native
+${PERL} Makefile.PL INSTALLDIRS=site
+make install
+popd
