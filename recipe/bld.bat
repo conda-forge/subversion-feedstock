73aca345a9ea488f440db767dcd44307e18c9da9
BHT <toby@anl.gov>
2022-05-21 11:08:32 -0500

    add more Windows dependencies

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -11,6 +11,7 @@ python gen-make.py -t vcproj --vsnet-version=%VS_YEAR% ^
              --with-apr-util=%LIBRARY_PREFIX% ^
              --with-apr-iconv=%LIBRARY_PREFIX% ^
              --with-sqlite=%LIBRARY_PREFIX% ^
+	     --with-py3c==%LIBRARY_PREFIX% ^
              --with-serf=%LIBRARY_INC% ^
              --release
 if errorlevel 1 exit 1
